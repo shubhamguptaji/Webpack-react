5c39235db3bc61bf17ddffc30ed34711820de3b0
shubhamguptaji <shubham2604gupta@gmail.com>
2018-12-09 18:49:09 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebPackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
